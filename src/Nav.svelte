b2f2262b7ca02bd3027f95f6b5ea6921fef55013
Melissa Jerome <melissa.jerome@wwt.com>
2021-03-04 21:08:00 -0600

    fix bug that breaks search after a bad search term

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -11,20 +11,25 @@
     
     
     function handleOnClick() {
-		console.log('searched');
+		console.log('searched', $searchError);
 		let search = searchValue.replaceAll(' ', '-').toLowerCase();
 		getMonsterByName(search).then(res => {
-			monster.set(res);
-			monsterDetail = $monster;
+            if(!$searchError) {
+                monster.set(res);
+			    monsterDetail = $monster;
+            }
 		}).then(() => {
             console.log(searchError);
-            let name = $monster.name
-	        let lowerName = name ? name.replaceAll(' ', '-').toLowerCase() : '';
-            navigate("/monster/" + lowerName, { replace: true });
-            console.log('Nav ', lowerName);
-            // console.log('should have navigated');
-			searchValue='';
-            console.log('monster onMount ', $monster);
+            if(!$searchError) {
+                let name = $monster.name
+                let lowerName = name ? name.replaceAll(' ', '-').toLowerCase() : '';
+                navigate("/monster/" + lowerName, { replace: true });
+                // console.log('Nav ', lowerName);
+                // console.log('should have navigated');
+                searchValue='';
+                console.log('monster onMount ', $monster);
+            }
+            
 		});
 		
 	};
@@ -37,7 +42,7 @@
             <input placeholder="Enter monster name" bind:value={searchValue} />
             <button on:click={handleOnClick} type="button"><img alt="search for a monster" src="/assets/search.svg"/></button>
         </form>
-        {#if $error}
+        {#if $searchError}
             <div>That monster does not exist. Please try another search.</div>
         {/if}
     </div>
